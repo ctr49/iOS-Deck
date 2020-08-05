7a3a0e3ec603aec6d1214fb3eb156a1e8cf008e0
Zac Gibson <zgibson@live.com>
2020-08-04 23:25:07 -0500

    Initial Commit

---
+++ iOS Deck/iOS_DeckApp.swift
@@ -0,0 +1,13 @@
+// Created for iOS Deck in 2020
+// Using Swift 5.0
+
+import SwiftUI
+
+@main
+struct iOS_DeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
